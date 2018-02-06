7aba876923a76e80d982204d4a60a5508af754f3
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 07:05:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,8 +46,8 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    name = filename.split("-")[1].split(".")[0][1..-1]
-    song = self.create_by_name(name)
+    song = self.new
+    song.name = filename.split("-")[1].split(".")[0][1..-1]
     song.artist_name = filename.split("-")[0][0..-2]
   end
 end
